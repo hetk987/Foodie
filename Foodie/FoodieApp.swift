be2403f2fe2f3b75bc0fef0ab4536f5ad3efa36e
hetk987 <66132321+hetk987@users.noreply.github.com>
2025-03-31 17:50:32 -0500

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Het Koradia on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodieTests/FoodieTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieTests.swift
+//  FoodieTests
+//
+//  Created by Het Koradia on 3/31/25.
+//
+
+import Testing
+@testable import Foodie
+
+struct FoodieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
